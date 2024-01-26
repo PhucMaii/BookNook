1cd63d5d2dcb89889fe94d77d52332ac5ef1e036
PhucMaii <maithienphuc0102@gmail.com>
2024-01-26 12:51:19 -0800

    Set up firebase and package.json

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,23 +1,23 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// import { getAuth, EmailAuthProvider } from "firebase/auth";
-// import { getFirestore } from 'firebase/firestore';
-// import { getStorage } from 'firebase/storage';
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getFunctions } from 'firebase/functions';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: import.meta.env.API_KEY,
-    authDomain: import.meta.env.AUTH_DOMAIN,
-    projectId: import.meta.env.PROJECT_ID,
-    storageBucket: import.meta.env.STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.MESSAGE_ID,
-    appId: import.meta.env.APP_ID,
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-// const emailprovider = new EmailAuthProvider();
-// const db = getFirestore(app);
-// const storage = getStorage ();
+initializeApp(firebaseConfig);
 
-export { app };
+const auth = getAuth();
+const googleProvider = new GoogleAuthProvider();
+const db = getFirestore();
+const storage = getStorage();
+const functions = getFunctions();
+export { auth, googleProvider, db, storage, functions };
