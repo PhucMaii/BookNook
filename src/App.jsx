2b1dcccb808250a995a23548629a8e0c302412ea
Chuanche Liu <000462312@student.vcc.ca>
2024-01-26 18:08:35 -0800

    Refs #13: Set up route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { BrowserRouter, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
 
