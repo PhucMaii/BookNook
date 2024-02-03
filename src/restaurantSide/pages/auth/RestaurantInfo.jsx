8332af03bab9757deb28fe177eae020175777376
Chuanche Liu <000462312@student.vcc.ca>
2024-02-02 19:43:08 -0800

    Refs #4: Authentication flow

--- src/restaurantSide/pages/auth/RestaurantInfo.jsx
+++ src/restaurantSide/pages/auth/RestaurantInfo.jsx
@@ -1,21 +1,39 @@
-import * as React from 'react'
+import React, { useState, useContext } from 'react'
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import {LoadingButton} from '@mui/lab';
 import Typography from '@mui/material/Typography';
 import Select from '@mui/material/Select'
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import RestaurantIcon from '@mui/icons-material/Restaurant';
+import SettingsCellIcon from '@mui/icons-material/SettingsCell';
 import { restaurantTypes, averagePrices } from '../../utils/constants'
 import { LogoImg, SideImg } from './styled';
+import { AuthContext } from '../../context/AuthContext';
+import { collection, getDocs, query, updateDoc, where } from '@firebase/firestore';
+import { db } from '../../../../firebaseConfig';
+import { Alert, Snackbar } from '@mui/material';
+import { useNavigate } from 'react-router';
 
 const RestaurantInformation = () => {
+    const [restaurantType, setRType] = useState('');
+    const [price, setPrice] = useState('');
+    const [restaurantName, setRestaurantName] = useState('');
+    const [contactNumber, setContactNumber] = useState('');
+    const [imgURL, setImgURL] = useState('');
+    const [notification, setNotification] = useState({
+        on: false,
+        severity: '',
+        message: ''
+    })
+    const [isLoading, setIsLoading] = useState(false);
 
-    const [restaurantType, setRType] = React.useState('');
-    const [price, setPrice] = React.useState('');
+    const navigate = useNavigate();
+
+    const { uid } = useContext(AuthContext);
 
     const handleRestaurantTypeChange = (event) => {
         setRType(event.target.value);
@@ -25,14 +43,61 @@ const RestaurantInformation = () => {
         setPrice(event.target.value);
     }
 
+    const handleUpdateInfo = async () => {
+        if (!restaurantName || !restaurantType || !contactNumber || !price) {
+            setNotification({
+                on: true,
+                severity: 'error',
+                message: 'Please fill out all the necessary field.'
+              })
+            return;
+          }
+
+        const submittedData = {
+            name: restaurantName,
+            type: restaurantType,
+            avgPrice: price,
+            phoneNumber: contactNumber,
+            imgURL
+        }
+
+        try {
+            setIsLoading(true)
+            const restaurantCollection = collection(db, 'restaurants');
+            const userQuery = query(restaurantCollection, where('uid', '==', uid));
+            const userSnapshot = await getDocs(userQuery); // There should be only 1 document/user
+            userSnapshot.forEach(async (doc) => {
+                await updateDoc(doc.ref, submittedData);
+            })
+            setNotification(
+                {
+                    on: true,
+                    severity: 'success',
+                    message: 'Updated info successfully.'
+                }
+            )
+            setIsLoading(false)
+            navigate('/overview')
+        } catch (error) {
+            console.log('Fail to update info: ', error);
+        }
+    }
+
     return (
         <Grid
             container
-            ColumnSpacing={2}
+            columnSpacing={2}
             justifyContent='center'
             overflow='hidden'
             height='100vh'
         >
+            <Snackbar
+                open={notification.on}
+                autoHideDuration={5000}
+                onClose={() => setNotification({ on: false })}
+            >
+                <Alert severity={notification.severity}>{notification.message}</Alert>
+            </Snackbar>
             <Grid item xs={6}>
                 <Box display='flex' flexDirection='column' margin='auto' width='50%'>
                     <Box display='flex' justifyContent='center' my={6}>
@@ -47,7 +112,6 @@ const RestaurantInformation = () => {
                     </Typography>
                     <Box display='flex' flexDirection='column' gap={3} mt={3}>
                         <TextField
-                            required
                             color="secondary"
                             id="outlined-required"
                             label="Restaurant Name"
@@ -56,6 +120,19 @@ const RestaurantInformation = () => {
                                     <RestaurantIcon />
                                 ),
                             }}
+                            onChange={(e) => setRestaurantName(e.target.value)}
+                        />
+                        <TextField
+                            id="outlined-required"
+                            label="Contact Number"
+                            color='secondary'
+                            InputProps={{
+                                startAdornment: (
+                                    <SettingsCellIcon />
+                                ),
+                            }}
+                            fullWidth
+                            onChange={(e) => setContactNumber(e.target.value)}
                         />
                         <FormControl fullWidth>
                             <InputLabel id="restaurant-type">Restaurant Type</InputLabel>
@@ -98,13 +175,22 @@ const RestaurantInformation = () => {
                             label="Restaurant Image"
                             InputProps={{
                             }}
+                            onChange={(e) => setImgURL(e.target.value)}
                         />
-                        <Button variant="contained" color='secondary'>Submit</Button>
+                        <LoadingButton 
+                            loading={isLoading}
+                            loadingIndicator="Updating Info..."
+                            variant="contained"
+                            color='secondary'
+                            onClick={handleUpdateInfo}
+                        >
+                            Submit
+                        </LoadingButton>
                     </Box>
                 </Box>
             </Grid>
             <Grid item xs={6}>
-                <SideImg src='/restaurantLoginImg.png' className='signupImage' alt="Login Img"/>
+                <SideImg src='/restaurantLoginImg.png' className='signupImage' alt="Login Img" />
             </Grid>
         </Grid>
     )
--- src/restaurantSide/pages/auth/SignUp.jsx
+++ src/restaurantSide/pages/auth/SignUp.jsx
@@ -1,19 +1,129 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
 import EmailIcon from '@mui/icons-material/Email';
 import KeyIcon from '@mui/icons-material/Key';
-import SettingsCellIcon from '@mui/icons-material/SettingsCell';
+import SaveIcon from '@mui/icons-material/Save';
 import { LogoImg, SideImg } from './styled';
 import { grey } from '@mui/material/colors';
+import { auth, db, googleProvider } from '../../../../firebaseConfig';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from '@firebase/auth';
+import { addDoc, collection } from '@firebase/firestore';
+import { Alert, Snackbar } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import { async } from '@firebase/util';
+import { AuthContext } from '../../context/AuthContext';
 
 const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [notification, setNotification] = useState({
+    on: false,
+    severity: '',
+    message: ''
+  })
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  const { setUid } = useContext(AuthContext);
+
+  const handleSignUp = async () => {
+    if (!email || !password || !confirmPassword) {
+      setNotification({
+          on: true,
+          severity: 'error',
+          message: 'Please fill out all the field.'
+        })
+      return;
+    }
+    
+    if (password !== confirmPassword) {
+      setNotification({
+          on: true,
+          severity:'error',
+          message:'Confirm password is unmatch.'
+        })
+      return;
+    }
+    
+    if (password.length < 6) {
+      setNotification({
+          on: true,
+          severity:'error',
+          message:'Password length is too short.'
+        })
+      return;
+    }
+
+    setIsLoading(true);
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const submittedData = {
+        email: userCredential.user.email,
+        uid: userCredential.user.uid
+      }
+      
+      const restaurantCollection = collection(db, 'restaurants');
+      await addDoc(restaurantCollection, submittedData);
+      
+      const userSignin = await signInWithEmailAndPassword(auth, email, password);
+      setUid(userSignin.user.uid);
+      
+      setNotification({
+          on: true,
+          severity:'success',
+          message:'Registered account successfully.'
+      })
+      setIsLoading(false);
+      navigate('/restaurant/create-info');
+    } catch (error) {
+      setIsLoading(false);
+      console.log('Fail to create user: ', error)
+      setNotification({
+          on: true,
+          severity:'error',
+          message:`Fail to create user: ${error.message}`
+        })
+    }
+  }
+
+  const handleGoogleSignup = async() => {
+    setIsLoading(true);
+    try {
+      const userCredential = await signInWithPopup(auth, googleProvider);
+      const submittedData = {
+        email: userCredential.user.email
+      }
+      const restaurantCollection = collection(db, 'restaurants');
+      await addDoc(restaurantCollection, submittedData);
+      setNotification(
+        {
+          on: true,
+          severity:'success',
+          message:'Registered account successfully.'
+        }
+      )
+      setIsLoading(false);
+      navigate('/restaurant/create-info')
+    } catch (error) {
+      setIsLoading(false);
+      console.log('Fail to create user: ', error)
+      setNotification(
+        {
+          on: true,
+          severity:'error',
+          message:`Fail to create user: ${error.message}`
+        }
+      )
+    }
+  }
+
   return (
     <Grid
       container
@@ -22,6 +132,13 @@ const SignUp = () => {
       overflow="hidden"
       height="100vh"
     >
+      <Snackbar
+        open={notification.on}
+        autoHideDuration={5000}
+        onClose={() => setNotification({on:false})}
+      >
+        <Alert severity={notification.severity}>{notification.message}</Alert>
+      </Snackbar>
       <Grid item xs={6}>
         <Box display="flex" flexDirection="column" margin="auto" width="50%">
           <Box display="flex" justifyContent="center" my={6}>
@@ -38,18 +155,6 @@ const SignUp = () => {
           <Box display="flex" flexDirection="column" gap={3} mt={3}>
             <TextField
               required
-              color="secondary"
-              id="outlined-required"
-              label="Restaurant Name"
-              InputProps={{
-                startAdornment: (
-                  <DriveFileRenameOutlineIcon />
-                ),
-              }}
-              fullWidth
-            />
-            <TextField
-              required
               id="outlined-required"
               label="Email Address"
               InputProps={{
@@ -58,6 +163,7 @@ const SignUp = () => {
                 ),
               }}
               fullWidth
+              onChange={(e) => setEmail(e.target.value)}
             />
             <TextField
               required
@@ -69,6 +175,7 @@ const SignUp = () => {
                 ),
               }}
               fullWidth
+              onChange={(e) => setPassword(e.target.value)}
             />
             <TextField
               required
@@ -80,33 +187,29 @@ const SignUp = () => {
                 ),
               }}
               fullWidth
+              onChange={(e) => setConfirmPassword(e.target.value)}
             />
-            <TextField
-              required
-              id="outlined-required"
-              label="Contact Number"
-              InputProps={{
-                startAdornment: (
-                  <SettingsCellIcon />
-                ),
-              }}
-              fullWidth
-            />
-            <Button variant="contained" color='secondary'>
+            <LoadingButton
+              loading={isLoading}
+              loadingIndicator="Registering User..."
+              variant="contained"
+              color='secondary'
+              onClick={handleSignUp}
+            >
               Create your Account
-            </Button>
+            </LoadingButton>
             <Divider variant='middle'>
               <Typography variant='body2'>Or</Typography>
             </Divider>
-            <Button variant="outlined" color='secondary'>
+            <Button variant="outlined" color='secondary' onClick={handleGoogleSignup}>
               <Box display="flex" gap={2} alignItems="center">
                 <img src='/icons/googleLogo.png' alt="Google Logo" />
                 <Typography>Sign in with Google</Typography>
               </Box>
             </Button>
           </Box>
-          <Typography variant="subtitle1" align='end' fontWeight='bold'>
-            Already have an account? 
+          <Typography variant="subtitle1" align='right' fontWeight='bold'>
+            Already have an account?
             <Link color="secondary" component='button'>
               Click here to sign in
             </Link>
