c2adda80dbd667b534666208dc261a54c31af241
PhucMaii <maithienphuc0102@gmail.com>
2024-02-06 11:20:57 -0800

    Suggesting Changes

---
+++ src/restaurantSide/components/HomepageCard/styled.jsx
@@ -0,0 +1,12 @@
+import { Card } from '@mui/material';
+import styled from 'styled-components';
+
+export const CardStyled = styled(Card)`
+    border-radius: 14px; 
+    max-width: 100%; 
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+`
+
+export const IconBackground = styled.div`
+
+`
