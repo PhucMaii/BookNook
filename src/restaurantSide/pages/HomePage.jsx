1fab23cce50bc4da45187d902b1a49aa7036c9b5
Chuanche Liu <000462312@student.vcc.ca>
2024-02-09 13:45:40 -0800

    Refs#10: Minor UI update, changed to select for filter

--- src/restaurantSide/pages/HomePage.jsx
+++ src/restaurantSide/pages/HomePage.jsx
@@ -13,6 +13,9 @@ import {
   InputAdornment,
   MenuItem,
   Menu,
+  Select,
+  InputLabel,
+  FormControl,
 } from '@mui/material';
 import React, { useState } from 'react';
 import Sidebar from '../components/Sidebar/Sidebar';
@@ -24,21 +27,13 @@ import { cardContent, tableContent } from '../utils/constants';
 import StatusText from '../components/StatusText/StatusText';
 
 export default function HomePage() {
-  const [anchorEl, setAnchorEl] = useState(null);
+
   const [filter, setFilter] = useState('');
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+  const handleSelect = (e) => {
+    setFilter(e.target.value)
+  }
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleSelect = (value) => {
-    setFilter(value);
-    handleClose();
-  };
 
   return (
     <Sidebar>
@@ -55,7 +50,7 @@ export default function HomePage() {
           ))}
         </Grid>
 
-        <Typography  variant='h5'>
+        <Typography variant='h5'>
           Today Reservations
         </Typography>
 
@@ -84,41 +79,38 @@ export default function HomePage() {
             </Grid>
 
             <Grid item xs={2}>
-              <Button
-                style={{ color: '#64748B' }}
-                startIcon={<FilterAltOutlinedIcon />}
-                aria-controls='filter-menu'
-                aria-haspopup='true'
-                onClick={handleClick}
-              >
-                {filter || 'Filter'}
-              </Button>
-              <Menu
-                id='filter-menu'
-                anchorEl={anchorEl}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={() => handleSelect('seated')}>
-                  Seated
-                </MenuItem>
-                <MenuItem onClick={() => handleSelect('confirmed')}>
-                  Confirmed
-                </MenuItem>
-                <MenuItem onClick={() => handleSelect('unseated')}>
-                  Unconfirmed
-                </MenuItem>
-                <MenuItem onClick={() => handleSelect('Newest to Oldest')}>
-                  Newest to Oldest
-                </MenuItem>
-                <MenuItem onClick={() => handleSelect('Oldest to Newest')}>
-                  Oldest to Newest
-                </MenuItem>
-                <MenuItem onClick={() => handleSelect('A to Z')}>A to Z</MenuItem>
-                <MenuItem onClick={() => handleSelect('Z to A')}>
-                  Z to A
-                </MenuItem>
-              </Menu>
+              <FormControl fullWidth>
+                <InputLabel id='filter' color='secondary'>Filter</InputLabel>
+                <Select
+                  color='secondary'
+                  labelId='filter'
+                  id='filter-select'
+                  value={filter}
+                  label='Filter'
+                  onChange={handleSelect}>
+                  <MenuItem value='Seated'>
+                    Seated
+                  </MenuItem>
+                  <MenuItem value='Confirmed'>
+                    Confirmed
+                  </MenuItem>
+                  <MenuItem value='Unconfirmed'>
+                    Unconfirmed
+                  </MenuItem>
+                  <MenuItem value='Newest to Oldest'>
+                    Newest to Oldest
+                  </MenuItem>
+                  <MenuItem value='Oldest to Newest'>
+                    Oldest to Newest
+                  </MenuItem>
+                  <MenuItem value='A to Z'>
+                    A to Z
+                  </MenuItem>
+                  <MenuItem value='Z to A'>
+                    Z to A
+                  </MenuItem>
+                </Select>
+              </FormControl>
             </Grid>
           </Grid>
 
@@ -138,8 +130,8 @@ export default function HomePage() {
                   <TableCell>{row.customerName}</TableCell>
                   <TableCell>{row.tableName}</TableCell>
                   <TableCell>{row.time}</TableCell>
-                  <TableCell style={{width:'20%'}}> 
-                    <StatusText text={row.status} type={row.status === 'seated' ? 'success' : row.status === 'Confirmed' ? 'warning' : 'error'}/>
+                  <TableCell style={{ width: '12%' }}>
+                    <StatusText text={row.status} type={row.status === 'seated' ? 'success' : row.status === 'Confirmed' ? 'warning' : 'error'} />
                   </TableCell>
                   <TableCell align='center'>
                     <Button variant="filled" style={{ color: '#64748B', backgroundColor: blueGrey }}>
